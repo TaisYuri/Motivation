18080d4dac937378f9a6f57e9797f51d669b9211
TaisYuri <taisyuri@hotmail.com>
2023-03-02 22:54:09 -0300

    commit initial

---
+++ app/src/main/java/com/example/motivation/ui/MainActivity.kt
@@ -0,0 +1,118 @@
+package com.example.motivation.ui
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.example.motivation.infra.MotivationConstants
+import com.example.motivation.R
+import com.example.motivation.infra.SecurityPreferences
+import com.example.motivation.databinding.ActivityMainBinding
+import com.example.motivation.databinding.ActivityUserBinding
+
+private lateinit var binding: ActivityMainBinding
+private var categoryId = 1
+
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //esconde a barra de navegação
+        supportActionBar?.hide()
+
+        //SharedPreferences (get)
+        val name = SecurityPreferences(this).getString(MotivationConstants.KEY.USER_NAME)
+        binding.textWelcome.text = "Olá $name!"
+
+        binding.buttonNewPhrase.setOnClickListener(this)
+
+
+        //Eventos das imagens
+        binding.imgAllInclusive.setOnClickListener(this)
+        binding.imgHappy.setOnClickListener(this)
+        binding.imgSunny.setOnClickListener(this)
+        binding.imgExitToApp.setOnClickListener(this)
+
+        //seta a primeira imagem vir selecionada
+        handleFilter(R.id.img_all_inclusive)
+
+    }
+
+    override fun onClick(v: View) {
+        if (v.id == R.id.buttonNewPhrase) {
+
+        } else if (v.id in listOf(
+                R.id.img_all_inclusive,
+                R.id.img_happy,
+                R.id.img_sunny,
+                R.id.img_exit_to_app
+            )
+        ) {
+            handleFilter(v.id)
+        }
+
+    }
+
+    private fun handleFilter(id: Int) {
+        binding.imgAllInclusive.setColorFilter(ContextCompat.getColor(this, R.color.img_background))
+        binding.imgHappy.setColorFilter(ContextCompat.getColor(this, R.color.img_background))
+        binding.imgSunny.setColorFilter(ContextCompat.getColor(this, R.color.img_background))
+        binding.imgExitToApp.setColorFilter(ContextCompat.getColor(this, R.color.img_background))
+
+        when (id) {
+            R.id.img_all_inclusive -> {
+                binding.imgAllInclusive.setColorFilter(
+                    ContextCompat.getColor(
+                        this,
+                        R.color.white
+                    )
+                )
+                categoryId = MotivationConstants.FILTER.ALL_INCLUSIVE
+            }
+            R.id.img_happy -> {
+                binding.imgHappy.setColorFilter(
+                    ContextCompat.getColor(
+                        this,
+                        R.color.white
+                    )
+                )
+                categoryId = MotivationConstants.FILTER.HAPPY
+            }
+            R.id.img_sunny -> {
+                binding.imgSunny.setColorFilter(
+                    ContextCompat.getColor(
+                        this,
+                        R.color.white
+                    )
+                )
+                categoryId = MotivationConstants.FILTER.SUNNY
+            }
+            R.id.img_exit_to_app -> {
+                binding.imgExitToApp.setColorFilter(
+                    ContextCompat.getColor(
+                        this,
+                        R.color.white
+                    )
+                )
+                handleSignOut() //EFETUAR LOGOFF
+            }
+        }
+
+    }
+
+    private fun handleSignOut() {
+        SecurityPreferences(this).removeString(MotivationConstants.KEY.USER_NAME)
+        Toast.makeText(this, "Logoff efetuado", Toast.LENGTH_SHORT).show()
+        startActivity(Intent(this, UserActivity::class.java))
+        finish()
+    }
+
+
+}
---
+++ app/src/main/java/com/example/motivation/ui/UserActivity.kt
@@ -0,0 +1,53 @@
+package com.example.motivation.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.motivation.infra.MotivationConstants
+import com.example.motivation.R
+import com.example.motivation.infra.SecurityPreferences
+import com.example.motivation.databinding.ActivityUserBinding
+
+private lateinit var binding: ActivityUserBinding
+
+class UserActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityUserBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+        
+        //Verifica se possui name salvo no storage, caso tenha, exibe a mainActivity
+        val name = SecurityPreferences(this).getString( MotivationConstants.KEY.USER_NAME)
+        if(name !== ""){
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+
+        binding.buttonSave.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View) {
+        if (v.id == R.id.button_save) {
+            handleSave()
+        }
+    }
+
+    private fun handleSave() {
+        val name = binding.editYourName.text.toString()
+        if (name != "") {
+
+            SecurityPreferences(this).storeString(MotivationConstants.KEY.USER_NAME, name)
+
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        } else {
+            Toast.makeText(this, R.string.validation_mandatory_name, Toast.LENGTH_SHORT).show()
+        }
+    }
+}
